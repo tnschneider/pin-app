0c12876ae5b8b769fda180a06c28896e4ac9f6ee
Terry Schneider <tschneider@propak.com>
2018-04-24 20:42:30 -0500

    minimize window to tray and add icons

--- app/src/main/app.js
+++ app/src/main/app.js
@@ -1,8 +1,9 @@
 const electron = require('electron');
-// Module to control application life.
+
 const app = electron.app;
-// Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
+const Tray = electron.Tray;
 
 const path = require('path');
 const url = require('url');
@@ -13,17 +14,31 @@ const {
 	REDUX_DEVTOOLS
 } = require('electron-devtools-installer');
 
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-/** This function will create the mainWindow */
 function createWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1024, height: 768});
 
-	// and load the index.html of the app.
+	mainWindow = new BrowserWindow({
+		width: 1024, 
+		height: 768,
+		title: "Pin App",
+		icon: './appicon256.png'
+	});
+
+	var tray = null;
+	tray = new Tray('./appicon64.png');
+	var contextMenu = Menu.buildFromTemplate([
+			{ label: 'Quit', click: () => {
+				app.isQuitting = true;
+        app.quit();
+			} }
+	]);
+	tray.setToolTip('Pin App');
+	tray.setContextMenu(contextMenu);
+	tray.on('click', () => {
+		mainWindow.show();
+	})
+
 	mainWindow.loadURL(url.format({
 		pathname: path.join(__dirname, 'index.html'),
 		protocol: 'file:',
@@ -31,7 +46,7 @@ function createWindow() {
 	}));
 
   if(process.env.NODE_ENV === 'development') {
-		// Open the DevTools.
+
 		mainWindow.webContents.openDevTools();
 		
 		installExtension(REACT_DEVELOPER_TOOLS)
@@ -42,38 +57,40 @@ function createWindow() {
 			.then((name) => console.log(`Added Extension:  ${name}`))
 			.catch((err) => console.log('An error occurred: ', err));
 
-  }
+	}
 
-  // Emitted when the window is closed.
+	//hide the app to the tray on minimize
+	mainWindow.on('minimize',function(event){
+    event.preventDefault();
+    mainWindow.hide();
+	});
+	
+	//hide the app to the tray on close
+	mainWindow.on('close', function (event) {
+    if(!app.isQuitting){
+        event.preventDefault();
+        mainWindow.hide();
+    }
+
+    return false;
+	});
+
   mainWindow.on('closed', function() {
-	// Dereference the window object, usually you would store windows
-	// in an array if your app supports multi windows, this is the time
-	// when you should delete the corresponding element.
-	mainWindow = null;
+		mainWindow = null;
   });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed.
+//leave the app running on mac os only
 app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
 		app.quit();
   }
 });
 
 app.on('activate', function() {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
 		createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
